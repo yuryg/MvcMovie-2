095446cedc3298f6540d25d46095a82e6a28ab8b
yuryg <city20box@yandex.ru>
2016-12-18 06:55:23 +0300

    Add project files.

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Тестируем передачц параметров через ViewBag
+        public ActionResult Welcome(string name, int numtimes = 1)
+        {
+            ViewBag.Message = "Hello, " + name;
+            ViewBag.NumTimes = numtimes;
+            return View();
+
+        }
+    }
+}
---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        [Display(Name="Дата выхода")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:dd-MM-yyyy}", ApplyFormatInEditMode =true)]
+        public DateTime ReleasedDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class MovieDbContext:DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
